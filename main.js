3ea75077d52414af3ec006af91af9742af3e52fd
priyankacute <bhavanimurugesh2000@gmail.com>
2023-07-19 15:57:21 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,7 @@
+var arr=[11,10,70,59,2,9];
+document.write("Given array is "+[11,10,70,59,2,9]+"<br>");
+sum=0;
+for(let i=0;i<arr.length;i++){
+								sum=sum+arr[i];
+								}
+							document.write(sum);
